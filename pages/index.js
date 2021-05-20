d04e9bfbbedb1dae5c5bd3b65211ded7302f1c47
Nathalia Laudano <nathalialaudano@crazytechlabs.com.br>
2021-05-20 19:05:07 -0300

    love letter

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
+/* 
+
+PRIMEIRO TRYOUT
+
 import { useState } from "react";
 
 function Home() {
@@ -20,6 +24,33 @@ function Contador() {
     <div>
       <div>{contador}</div>
       <buttom onClick={adicionarContador}>Adiciona</buttom>
+    </div>
+  );
+}
+
+export default Home;
+
+/*/
+
+function Home() {
+  return (
+    <div
+      style={{
+        margin: "100px auto",
+        border: "1px dotted #ccc",
+        width: "300px",
+      }}
+    >
+      <div style={{ margin: "0 auto", width: "82%" }}>
+        <h1 style={{ fontFamily: "monospace" }}>💌 Mozão,</h1>
+        <p style={{ fontFamily: "monospace" }}>
+          Te amo muito.. até em react 😍
+        </p>
+        <p style={{ fontFamily: "monospace" }}>
+          Obrigada por ser boazinha de novo!
+        </p>
+        <p style={{ fontFamily: "monospace" }}>Beijo da favorita 😎</p>
+      </div>
     </div>
   );
 }
